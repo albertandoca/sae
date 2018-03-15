c640141551f19e3ddcf21cffc79f379ba95c4098
JosseAguirre <36019732+JosseAguirre@users.noreply.github.com>
2018-03-15 14:47:35 -0500

    Delete correquisito.sql

--- BDD/correquisito.sql
+++
@@ -1,4 +0,0 @@
-Use ignug;
-Select Asignatura.nombre, AsignaturaCorrequisito.id from AsignaturaCorrequisito
-Inner Join Asignatura on AsignaturaCorrequisito.idAsignaturaCorrequisito = Asignatura.id
-Where AsignaturaCorrequisito.idAsignatura;
