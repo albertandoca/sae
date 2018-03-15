df4bb15150e83a024019a82fa1027b38e00b77ca
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2018-03-15 11:48:53 -0500

    Eliminacion de archivos repetidos

--- BDD/prerequisitos.sql
+++
@@ -1,6 +0,0 @@
-Use ignug;
-Select Asignatura.nombre, AsignaturaPrerrequisito.id from AsignaturaPrerrequisito
-Inner Join Asignatura on AsignaturaPrerrequisito.idAsignaturaPrerrequisito = Asignatura.id
-Where AsignaturaPrerrequisito.idAsignatura;
-
-
--- BDD/silaboElemnetos.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM ignug.SilaboElementos;
-insert into SilaboElementos(idSilaboUnidades, verbo, objeto ,condicion, finalidad, codigo) values
-(1, 'Conocer', 'los conjuntos', 'de una manera correcta','para aplicar en la programacion', 1.1);
-insert into SilaboElementos(idSilaboUnidades, verbo, objeto ,condicion, finalidad, codigo) values
-(7, 'Aplicar', 'la programacion orientada a objetos', 'basada en varias técnicas, incluyendo herencia, cohesión, abstracción, polimorfismo, acoplamiento y encapsulamiento.','con la finalidad de obtener buenos programadores ', 1.1);
