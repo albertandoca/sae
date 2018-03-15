2a17d03a7fa232119da39585a14b8ee609b7174e
JosseAguirre <36019732+JosseAguirre@users.noreply.github.com>
2018-03-15 14:48:48 -0500

    Delete malla.sql

--- BDD/malla.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM ignug.PerfilEgreso;
-Insert Into PerfilEgreso (idmalla,detalle) Values (1,'Aprobado');
-Insert Into PerfilEgreso (idmalla,detalle) Values (1,'Aprobado');
-Insert Into PerfilEgreso (idmalla,detalle) Values (2,'Aprobado');
-Insert Into PerfilEgreso (idmalla,detalle) Values (3,'Aprobado');
-Insert Into PerfilEgreso (idmalla,detalle) Values (4,'Aprobado');
-Insert Into PerfilEgreso (idmalla,detalle) Values (5,'Aprobado');
-Insert Into PerfilEgreso (idmalla,detalle) Values (6,'Aprobado');
-describe PerfilEgreso
