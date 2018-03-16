a185a13bd10263ea59fcd4d8a93689240b69ef83
JosseAguirre <36019732+JosseAguirre@users.noreply.github.com>
2018-03-16 14:33:15 -0500

    Delete prerequisitos.sql

--- BDD/prerequisitos.sql
+++
@@ -1,6 +0,0 @@
-Use ignug;
-Select Asignatura.nombre, AsignaturaPrerrequisito.id from AsignaturaPrerrequisito
-Inner Join Asignatura on AsignaturaPrerrequisito.idAsignaturaPrerequisito = Asignatura.id
-Where AsignaturaPrerrequisito.idAsignatura;
-
-
