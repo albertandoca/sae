987893f5e2ee3bf1eaf7c688ffe0fed2a2c28c6c
JosseAguirre <36019732+JosseAguirre@users.noreply.github.com>
2018-03-16 14:33:42 -0500

    Delete metodologia.sql

--- BDD/metodologia.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM ignug.Metodologia;
-insert into Metodologia(id, estrategia,finalidad) Values( 1,'Curso Angular','Tene bases para desarollar programas');
-insert into Metodologia(id, estrategia,finalidad) Values( 2,'Null','Null');
-insert into Metodologia(id, estrategia,finalidad) Values( 3,'Null','Null');
-insert into Metodologia(id, estrategia,finalidad) Values( 4,'Null','Null');
-insert into Metodologia(id, estrategia,finalidad) Values( 5,'Null','Null');
-
