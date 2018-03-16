b97f6e97090f58d8dc452f8b1c5c2eab5d12decd
JosseAguirre <36019732+JosseAguirre@users.noreply.github.com>
2018-03-16 14:37:05 -0500

    Delete aulas.sql

--- BDD/aulas.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM ignug.Aula;
-Insert into Aula (capacidad,descripcion,idTipoAula) values (50,'Cuenca',1),(35,'Catamarca',1),(40,'Cueva de las Manos',1),
-(38,'Xian',2), (25,'Yasuni',1), (27,'Maracaibo',1), (23,'Touluse',2),(20,'Gori',2),(25,'Montecristi',1), (25,'Galapagos',1)
