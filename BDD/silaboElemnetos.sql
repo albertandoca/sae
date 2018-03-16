54af36dbbb6569185d9f976498563ebb0765bf61
JosseAguirre <36019732+JosseAguirre@users.noreply.github.com>
2018-03-16 14:32:50 -0500

    Delete silaboElemnetos.sql

--- BDD/silaboElemnetos.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM ignug.SilaboElementos;
-insert into SilaboElementos(idSilaboUnidades, IdVerboBloom, objeto ,condicion, finalidad, codigo) values
-(1, 2, 'los conjuntos', 'de una manera correcta','para aplicar en la programacion', 1.1);
-insert into SilaboElementos(idSilaboUnidades, IdVerboBloom, objeto ,condicion, finalidad, codigo) values
-(7, 3, 'la programacion orientada a objetos', 'basada en varias técnicas, incluyendo herencia, cohesión, abstracción, polimorfismo, acoplamiento y encapsulamiento.','con la finalidad de obtener buenos programadores ', 1.1);
